8e6c62a8ec08961baa692b26ec9ea6c48be790ab
Palmer Lao <palmer.lao@gmail.com>
2017-06-04 19:19:45 -0700

    add touchscreen scripts for x230t

---
+++ bound_touch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xsetwacom set "Wacom ISDv4 E6 Pen stylus" MapToOutput LVDS-1
+xsetwacom set "Wacom ISDv4 E6 Finger touch" MapToOutput LVDS-1
+xsetwacom set "Wacom ISDv4 E6 Pen eraser" MapToOutput LVDS-1
---
+++ rotate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xrandr --output LVDS-1 --rotate inverted
+xsetwacom set "Virtual core XTEST pointer" rotate half
+xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate half
+xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate half
+xsetwacom set "SynPS/2 Synaptics Touchpad" rotate half
+xsetwacom set "TPPS/2 IBM TrackPoint" rotate half
+xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate half
+xsetwacom set "PixArt USB Optical Mouse" rotate half
